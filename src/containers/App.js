eec2535f667b52296b4682428caa053cb8df8239
TienAnh <buitienanhngu@gmail.com>
2023-04-16 22:31:38 +0700

    Fix Change language

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,13 +7,10 @@ import { ToastContainer } from 'react-toastify';
 import { userIsAuthenticated, userIsNotAuthenticated } from '../hoc/authentication';
 import { path } from '../utils'
 import Home from '../routes/Home';
-// import Login from '../routes/Login';
 import Login from './Auth/Login';
-import Header from './Header/Header';
 import System from '../routes/System';
 import { CustomToastCloseButton } from '../components/CustomToast';
 import HomePage from './HomePage/HomePage.js';
-
 import CustomScrollbars from '../components/CustomScrollbars';
 
 
@@ -43,7 +40,6 @@ class App extends Component {
                 <Router history={history}>
                     <div className="main-container">
 
-                        {this.props.isLoggedIn && <Header />}
 
                         <div className="content-container">
                             <CustomScrollbars style={{ height: '100vh', while: '100%' }}>
