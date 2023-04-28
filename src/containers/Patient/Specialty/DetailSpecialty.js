93d804e4a66d55840918a0c477d63123d8a0a388
TienAnh <buitienanhngu@gmail.com>
2023-04-28 11:27:07 +0700

    Done function detail specialty

--- src/containers/Patient/Specialty/DetailSpecialty.js
+++ src/containers/Patient/Specialty/DetailSpecialty.js
@@ -44,10 +44,23 @@ class DetailSpecialty extends Component {
 
                     }
                 }
+
+                let dataPrvince = resProvince.data;
+                let result = [];
+                if (dataPrvince && dataPrvince.length > 0) {
+                    dataPrvince.unshift({
+                        createAt: null,
+                        keyMap: 'ALL',
+                        type: 'PROVINCE',
+                        valueEn: 'ALL',
+                        valueVi: 'Toàn quốc',
+                    })
+                }
+
                 this.setState({
                     dataDetailSpecialty: res.data,
                     arrDoctorId: arrDoctorId,
-                    listProvince: resProvince.data
+                    listProvince: dataPrvince ? dataPrvince : []
                 })
             }
         }
@@ -56,8 +69,32 @@ class DetailSpecialty extends Component {
     componentDidUpdate(prevProps, prevState, snapshot) {
 
     }
-    handleOnchangeSelect = (event) => {
+    handleOnchangeSelect = async (event) => {
+        if (this.props.match && this.props.match.params && this.props.match.params.id) {
+            let id = this.props.match.params.id;
+            let location = event.target.value;
+            let res = await getAllDetailSpecialtyById({
+                id: id,
+                location: location
+            });
 
+            if (res && res.errCode === 0) {
+                let data = res.data;
+                let arrDoctorId = [];
+                if (data && !_.isEmpty(res.data)) {
+                    let arr = data.doctorSpecialty;
+                    if (arr && arr.length > 0) {
+                        arr.map(item => {
+                            arrDoctorId.push(item.doctorId)
+                        })
+                    }
+                }
+                this.setState({
+                    dataDetailSpecialty: res.data,
+                    arrDoctorId: arrDoctorId,
+                })
+            }
+        }
     }
 
     render() {
@@ -98,6 +135,8 @@ class DetailSpecialty extends Component {
                                             <ProfileDoctor
                                                 doctorId={item}
                                                 isShowDescriptionDoctor={true}
+                                                isShowLinkDetail={true}
+                                                isShowPrice={false}
                                             />
                                         </div>
                                     </div>
