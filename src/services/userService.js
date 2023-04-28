a4e8573b46cc317c3b00c975510da1a2b07dfd38
TienAnh <buitienanhngu@gmail.com>
2023-04-28 12:43:11 +0700

    Create data clinic

--- src/services/userService.js
+++ src/services/userService.js
@@ -86,6 +86,10 @@ const getAllDetailSpecialtyById = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`)
 }
 
+const createNewClinic = (data) => {
+    return axios.post('/api/create-new-clinic', data)
+}
+
 export {
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUservice,
@@ -95,5 +99,6 @@ export {
     getScheduleDoctorByDate, getExtraInforDoctorById,
     getProfileDoctorById, postPatientBookAppointment,
     postVerifyBookAppointment, createNewSpecialty,
-    getAllSpecialty, getAllDetailSpecialtyById
+    getAllSpecialty, getAllDetailSpecialtyById,
+    createNewClinic
 }
