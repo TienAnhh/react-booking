6e53001c8cdb29d3373d03614c947a3a709eed25
TienAnh <buitienanhngu@gmail.com>
2023-04-28 16:57:12 +0700

    Base manage booking

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -73,6 +73,9 @@ export const doctorMenu = [
             {
                 name: 'menu.doctor.manage-schedule', link: '/doctor/manage-schedule'
             },
+            { // 
+                name: 'menu.doctor.manage-patient', link: '/doctor/manage-patient'
+            },
         ]
     }
 ];
